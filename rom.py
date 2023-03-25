606c51792b39648669a4f445d9a9f07be1b75719
pinkpuff <kalel.of.krypton@gmail.com>
2023-03-25 14:15:17 -0230

    Added comments

--- rom.py
+++ rom.py
@@ -4,7 +4,8 @@ class RomData:
   self.data = data
 
   # Constants representing locations of various data in the rom.
-  # Ordering them by address helps with debugging.
+  # Ordering them by address helps with debugging and makes it more useful as a
+  # general reference resource.
   self.MONSTER_NAMES_START =      0x71A00
   self.MONSTER_GP_START =         0x72200
   self.MONSTER_XP_START =         0x723C0
@@ -37,8 +38,8 @@ class RomData:
   self.TILEMAP_POINTERS_START =   0xB8200
   self.TILEMAP_DATA_START =       0xB8500
   
-
-  # Constants representing other values or quantities.
+  # Constants representing other values or quantities related to reading and writing
+  # data to and from the rom.
   self.TOTAL_ATTRIBUTE_TABLES = 128
   self.TOTAL_SPELLS = 0x48
   self.TOTAL_SPELLBOOKS = 13
@@ -69,45 +70,56 @@ class RomData:
   self.TILEMAP_OVERWORLD_BONUS = 0xB8200
   self.TILEMAP_UNDERMOON_BONUS = 0xC0200
 
+ # This directly injects a sequence of bytes into the romdata, starting
+ # at the given address. No safety checks or any other kind of checks
+ # are made; whatever data used to be at that location is blindly
+ # clobbered and replaced by the new bytes.
  def inject(self, address, bytelist):
-  # This directly injects a sequence of bytes into the romdata, starting
-  # at the given address. No safety checks or any other kind of checks
-  # are made; whatever data used to be at that location is blindly
-  # clobbered and replaced by the new bytes.
   for index, byte in enumerate(bytelist):
    self.data[address + index] = byte
 
+ # This returns a boolean value based on whether the given bit of the
+ # given byte is set or not.
  def flag(self, address, bit):
-  # This returns a boolean value based on whether the given bit of the
-  # given byte is set or not.
   return True if (self.data[address] >> bit) & 1 else False
 
+ # This sets the given bit of the byte at the given address.
+ # If the "value" parameter is specified, it will instead set or unset
+ # the bit based on whether the value is true or false.
  def setbit(self, address, bit, value = True):
-  # This sets the given bit of the byte at the given address.
-  # If the "value" parameter is specified, it will instead set or unset
-  # the bit based on whether the value is true or false.
   if value:
    self.data[address] = self.data[address] | (1 << bit)
   else:
    self.data[address] = self.data[address] & (0xFF ^ (1 << bit))
  
+ # This reads a 16-bit (or 24-bit etc) value starting at the given
+ # address and returns it as an integer.
  def read_wide(self, address, width = 2):
-  # This reads a 16-bit (or 24-bit etc) value starting at the given
-  # address and returns it as an integer.
   result = 0
   for index in range(width):
    result += self.data[address + index] * (0x100 ** index)
   return result
  
+ # This writes the given value at the given address and the following
+ # addresses as needed. It will not exceed the given width in bytes.
+ # Whatever data is at those following addresses will be completely
+ # overwritten and forgotten.
  def write_wide(self, address, number, width = 2):
-  # This writes the given value at the given address and the following
-  # addresses as needed. It will not exceed the given width in bytes.
-  # Whatever data is at those following addresses will be completely
-  # overwritten and forgotten.
   for index in range(width):
    self.data[address + index] = (number >> (8 * index)) % 0x100
 
+ # This applies an IPS patch to the rom.
+ # You have to indicate as a parameter whether or not the patch in question expects
+ # a headered rom. Unfortunately, there's no real way to determine for an arbitrary
+ # IPS patch whether it needs a headered rom or an unheadered one; you would have
+ # to just already know, or see if its creator documented that information, or
+ # figure it out by trial and error.
  def apply_patch(self, filename, headered_rom_expected = True):
+  
+  # You can either pass the header parameter a True/False value, in which case True
+  # means "yes, it expects a header" and False means "no, it expects it to have no
+  # header", or you can alternatively pass a string such as "headered" or
+  # "headerless".
   if type(headered_rom_expected) == str:
    if headered_rom_expected in ["headered", "header"]:
     headered_rom_expected = True
@@ -115,37 +127,58 @@ class RomData:
     headered_rom_expected = False
    else:
     print("ERROR: Unrecognized value for 'headered_rom_expected' in 'apply_patch' function.")
+
+  # First we read the bytes in the patch file.
   with open(filename, "rb") as patchfile:
    patch = patchfile.read()
+  
+  # All IPS files start with "PATCH" in ASCII and end with "EOF". Thus, any patch
+  # file whose length is less than 8 is automatically invalid.
   if len(patch) >= 8:
    header = ""
    for index in range(5):
     header += chr(patch[index])
    done = False
+   
+   # We use this "head" variable to track what position in the file we're looking
+   # at. (It acts much like a "read head" of a disk.)
    head = 5
+   
+   # Loop through the file looking for "chunks" of three bytes which usually
+   # indicate the next address at which to inject data. If those three bytes are
+   # the ASCII encoding of "EOF" though, then that indicates we're done and can
+   # exit the loop.
    while not done:
+   
+    # Look at the next three bytes and move the head accordingly.
     offset = patch[head:head + 3]
     head += 3
+    
+    # Check for the "EOF" signal. If it matches, we exit the loop.
     if offset[0] == 0x45 and offset[1] == 0x4F and offset[2] == 0x46:
      done = True
+     
+    # Otherwise, we treat it as the next address at which to change bytes.
     else:
      address = offset[0] * 0x10000 + offset[1] * 0x100 + offset[2]
      if not headered_rom_expected:
       # In this situation, the patch expects an UNHEADERED rom but the
-      # roms in this library are all HEADERED. Even if an unheadered
+      # roms in this library are *all* HEADERED. Even if an unheadered
       # rom was loaded, a blank header is added in order to make sure
       # the addresses line up for other things. So if the patch was
       # made for an unheadered rom, we need to add the size of the
       # header to the address in question in order to line it up with
       # the "headered" rom in memory.
-      # There's no real way to determine for an arbitrary IPS patch
-      # whether it needs a headered rom or an unheadered one; you would
-      # have to just already know, or see if its creator documented
-      # that information, or by trial and error.
       address += 0x200
+     
+     # The next two bytes of each chunk indicate the number of bytes in the target
+     # rom to change.
      length = patch[head:head + 2]
      head += 2
      total = length[0] * 0x100 + length[1]
+     
+     # Finally, the rest of the chunk should consist of the aforementioned number of
+     # bytes which define the changed data.
      patchstring = ""
      patchbytes = []
      if total > 0:
@@ -153,14 +186,23 @@ class RomData:
        patchstring += "{:02x}, ".format(byte)
        patchbytes.append(byte)
       head += total
+      
+     # There are two ways a chunk can be defined. It can either be a specified
+     # number of bytes, or it can be RLE encoded. If a particular chunk is RLE
+     # encoded, the two length bytes will both be 0.
      else:
       runlength = patch[head] * 0x100 + patch[head + 1]
       runbyte = patch[head + 2]
       head += 3
       patchstring = "{:02x}, ".format(runbyte) * runlength
       patchbytes = [runbyte] * runlength
+     
+     # Finally we have pieced together the data that should be injected and where
+     # to inject it, so now we can simply write them accordingly.
      for index, byte in enumerate(patchbytes):
       self.data[address + index] = byte
+
+  # The IPS file was less than 8 bytes long so evidently it is invalid.
   else:
    print("Invalid ips file: {}".format(filename))
 
--- text.py
+++ text.py
@@ -1,13 +1,26 @@
 class TextInterface:
 
  def __init__(self):
+  
+  # This array converts symbols in the FF4 text encoding into ASCII.
   self.ff4totext = {}
+  
+  # We start by populating the array with the hex representation of each symbol.
   for i in range(0x100):
    self.ff4totext[chr(i)] = "[{}]".format(self.hex(i))
+  
+  # Then we replace the capital letters with their ASCII equivalents.
   for i in range(0x42, 0x5C):
    self.ff4totext[chr(i)] = chr(i - 1)
+  
+  # And likewise the lowercase letters.
   for i in range(0x5C, 0x76):
    self.ff4totext[chr(i)] = chr(i + 0x61 - 0x5C)
+  
+  # Then we take care of some individual symbols. Punctuation is straightforward
+  # enough, but some symbols are unique to FF4 and do not have ASCII equivalents.
+  # For these, we use three letters enclosed in square brackets to reperesent a
+  # single symbol. This improves readability without bloating the length too much.
   self.ff4totext[chr(0x15)] = "#"
   self.ff4totext[chr(0x29)] = "[CLW]"
   self.ff4totext[chr(0x2A)] = "[ROD]"
@@ -62,36 +75,50 @@ class TextInterface:
   self.ff4totext[chr(0xC8)] = ":"
   self.ff4totext[chr(0xC9)] = ","
   self.ff4totext[chr(0xFF)] = " "
+  
+  # Then we use that array to create the inverse array, which converts ASCII text
+  # back into FF4 encoded text.
   self.texttoff4 = {value: key for key, value in self.ff4totext.items()}
 
+ # This returns a string representation of a hex value, including one leading 0 for
+ # single-digit values.
  def hex(self, number):
   return "{:02x}".format(number).upper()
 
+ # This takes a string of text in FF4 encoding and converts it to ASCII.
  def asciitext(self, ff4string):
-  # Takes a string of text in FF4 encoding and converts it to ASCII.
   result = ""
+  
+  # Since FF4 encoding uses one byte per symbol, we can simply use the array to
+  # construct the string byte by byte.
   for letter in ff4string:
    result += self.ff4totext[letter]
+  
+  # And finally return it.
   return result
 
+ # This takes a string of ASCII text and converts it to FF4 encoding.
  def ff4text(self, asciistring):
-   # Takes a string of ASCII text and converts it to FF4 encoding.
-   result = ""
-   codemode = False
-   code = ""
-   for letter in asciistring:
-    # When converting ASCII text, we need to consider things between [] as a single symbol.
-    if codemode:
-     code += letter
-     if letter == "]":
-      codemode = False
-      result += self.texttoff4[code]
-    elif letter == "[":
-     code = "["
-     codemode = True
-    else:
-     result += self.texttoff4[letter]
-   return result
+  result = ""
+  codemode = False
+  code = ""
+
+  # When converting ASCII text, we need to consider things between [] as a single
+  # symbol. Thus, if we read a "[", we change to a special "code-reading mode"
+  # until we read a "]". Then the code is looked up and the proper symbol added to
+  # the resulting string and we return back to "letter-reading mode".
+  for letter in asciistring:
+   if codemode:
+    code += letter
+    if letter == "]":
+     codemode = False
+     result += self.texttoff4[code]
+   elif letter == "[":
+    code = "["
+    codemode = True
+   else:
+    result += self.texttoff4[letter]
+  return result
 
  def from_bytes(self, bytelist):
   result = ""
@@ -105,15 +132,13 @@ class TextInterface:
    result.append(ord(letter))
   return result
 
+ # Assigns a new ascii symbol to a given hex code.
+ # This is useful for displaying text in roms whose text encoding has been altered.
+ # Note that this will not automatically go through all the text in all the
+ # abstract game objects that have already been created and replace the symbol. It
+ # is best to call this function BEFORE calling read if you want the already read
+ # text to reflect the change.
  def assign_symbol(self, code, symbol):
-  # Assigns a new ascii symbol to a given hex code.
-  # This is useful for displaying text in roms whose text encoding has
-  # been altered.
-  # 
-  # Note that this will not automatically go through all the text in
-  # any abstract game objects that have already been created and
-  # replace the symbol. It is best to call this function BEFORE calling
-  # read if you want the already read text to reflect the change.
   self.ff4totext.update({chr(code): symbol})
   self.texttoff4.update({symbol: chr(code)})
  
