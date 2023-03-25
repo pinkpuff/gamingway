1054f0832330b47c304fdc28ec2106af3add6e12
pinkpuff <kalel.of.krypton@gmail.com>
2023-03-25 10:55:41 -0230

    Added more comments

--- world.py
+++ world.py
@@ -1,18 +1,19 @@
+# This represents the actual grid of tiles that compose a map. The reason this is
+# its own separate class and not part of the Map itself is because multiple Maps
+# can share the same TileMap, in which case the shared TileMap is only stored once
+# and simply pointed to by the various Maps that use it.
 class TileMap:
- # This represents the actual grid of tiles that compose a map. The reason this is
- # its own separate class and not part of the Map itself is because multiple Maps
- # can share the same TileMap, in which case the shared TileMap is only stored once
- # and simply pointed to by the various Maps that use it.
 
  def __init__(self):
+
   # The "tiles" variable represents a 2D array of tile indexes.
   self.tiles = []
  
+ # This reads the RLE encoded tile data for a single TileMap from the rom, starting
+ # at the specified address, and continuing until 0x400 tiles have been read.
+ # During the tile reading process, the tiles are read into a 1D array which is
+ # then converted to a 32x32 2D array.
  def read(self, rom, address):
-  # This reads the RLE encoded tile data for a single TileMap from the rom, starting
-  # at the specified address, and continuing until 0x400 tiles have been read.
-  # During the tile reading process, the tiles are read into a 1D array which is
-  # then converted to a 32x32 2D array.
   tiles = []
   offset = 0
 
@@ -40,16 +41,22 @@ class TileMap:
   # Finally, once we've read a full map worth of tiles, we convert it to a 2D array.
   self.tiles = [[tiles[y * 32 + x] for x in range(32)] for y in range(32)]
 
+ # This writes the 2D tile array back to the rom as an RLE encoded sequence.
+ # Most of the work though is done by the "encode" method.
  def write(self, rom, address):
-  # This writes the 2D tile array back to the rom as an RLE encoded sequence.
-  # Most of the work though is done by the "encode" method.
+ 
+  # The encode method does the heavy lifting.
   encoding = self.encode()
+  
+  # We simply inject it directly into the appropriate place in the rom.
   rom.inject(address, encoding)
+  
+  # And return the new address based on how long the encoding was.
   return address + len(encoding)
 
+ # This converts the TileMap object's 2D tiles array into a 1D array of tiles
+ # using run-length encoding.
  def encode(self):
-  # This converts the TileMap object's 2D tiles array into a 1D array of tiles
-  # using run-length encoding.
   result = []
   tiles = [tile for row in self.tiles for tile in row]
 
@@ -120,41 +127,144 @@ class TileMap:
   # And finally, return the constructed RLE encoded array of bytes.
   return result
 
+# This refers to an entire town or dungeon floor, including its associated
+# metadata such as whether it's magnetic, whether you can use Warp or Exit,
+# what music plays, etc. It does NOT contain the layout of tiles (at least
+# not directly; it instead contains an index referencing one of the TileMaps).
 class Map:
- # This refers to an entire town or dungeon floor, including its associated
- # metadata such as whether it's magnetic, whether you can use Warp or Exit,
- # what music plays, etc. It does NOT contain the layout of tiles, at least
- # not directly. Rather, it contains an index of a TileMap object as defined
- # above.
 
  def __init__(self):
+
+  # Triggers are essentially tiles which activate some kind of effect when the
+  # player steps on them or interacts with them. Treasures, events, and teleports
+  # can all be caused by interacting with a trigger tile on a map. Each map has a
+  # list of Trigger objects that encode this information.
   self.triggers = []
+  
+  # Each map also has associated with it a set of messages or dialogues that can be
+  # referenced by NPCs or events in that map. The point of doing it this way is so
+  # that you can have one NPC event that simply says "Display map message 3" or
+  # something similar and then you can reuse that same NPC event in every map while
+  # having the actual dialogue spoken be different for different maps.
   self.messages = []
+  
+  # The backdrop refers to the battle background that will be used for any combat
+  # encounters that happen within the map.
   self.backdrop = 0
+  
+  # These indicate whether the "Warp" spell and "Exit" spell respectively can be
+  # used on this map.
   self.warpable = False
   self.exitable = False
+  
+  # Some maps have an "alternate version" of a battle background, such as the Sylph
+  # Cave background being an alternate version of the Cave of Summons background.
+  # This flag indicates whether or not to use the alternate version, if it exists.
   self.alternate_backdrop = False
+  
+  # When this flag is set, it applies the Magnetized status to all allies in
+  # battles spawned from this map.
   self.magnetic = False
+  
+  # The tilemap doesn't represent the actual array of tiles but rather indicates
+  # the INDEX of which TileMap to use for this map. This allows multiple maps to use
+  # the same layout of tiles, trigger tile locations, etc, but have different NPCs,
+  # messages, and different effects for the triggers.
   self.tilemap = 0
+  
+  # The tileset likewise indicates the index of which set of tiles to use. For
+  # example, the castle tileset is different from the town tileset, which is
+  # different from the cavern tileset, and so on.
   self.tileset = 0
+  
+  # A layout is an arrangement of NPCs. It indicates which NPCs are present and at
+  # what locations within the map. There is a separate list of these layouts, and
+  # this variable indicates which of those NPC layouts is used by this map.
   self.layout = 0
+  
+  # When displaying the map, sometimes the camera is positioned in such a way that
+  # it shows some area outside the defined TileMap. This indicates which tile to
+  # fill that area with.
   self.border_tile = 0
+  
+  # I'm not entirely certain what this means; I just copied it directly from
+  # FF4kster. I suspect if this flag is set, the tiles outside the area defined by
+  # the TileMap are considered solid, even if the border tile above would normally
+  # be non-solid.
   self.solid_border = False
+  
+  # This indicates which set of colors to use for the tileset.
   self.palette = 0
+  
+  # This indicates which set of colors to use for this map's NPCs. Even maps with
+  # the same NPC layout can have differently colored NPCs. There are two entries in
+  # this list; some NPCs use palette 1 and some use palette 2. I think this just
+  # depends on which sprite the NPC uses; some sprites are tied to palette 1 while
+  # others are tied to palette 2.
   self.npc_palettes = [0, 0]
+  
+  # The BGM track that normally plays on this map.
   self.music = 0
+  
+  # Some maps show another map in the background. For exmaple, in the upper levels
+  # of the Watery Pass, you can see a small river below the chasm, or in the Town
+  # of Summons you can see previous floors through the empty areas of the map. I
+  # think it's also used to achieve certain other effects like the Mist Cave's mist.
+  # I think this is a tileset index as opposed to a map index but I have yet to
+  # verify that.
   self.background = 0
+  
+  # This indicates that the background map above should be displayed in a semi-
+  # transparent way, such as the aforementioned mist effect.
   self.translucent = False
+  
+  # I think these indicate whether the background map uses the parallax style
+  # scrolling in each direction or whether it's simply "glued" to the position of
+  # the map's main tileset as it scrolls.
   self.scroll_vertical = False
   self.scroll_horizontal = False
+  
+  # I haven't investigated yet what this flag represents, if anything.
   self.mystery_bit = False
+  
+  # Some background maps scroll automatically, even while the main map stays still.
+  # Examples include the Mist Cave's mist or the water in the town maps. I'm not yet
+  # sure which numbers indicate which direction.
   self.move_direction = 0
+  
+  # This indicates the speed of the background map's automatic scroll.
   self.move_speed = 0
+  
+  # This flag seems to be set on maps used in the ending sequence, but beyond that,
+  # I'm not really sure what else it does, if anything.
   self.ending = False
+  
+  # Map names are not stored directly but rather as an index in a list of names. I
+  # suspect this is because a lot of maps are simply "B1", "F2", etc, and not many
+  # have unique names; indeed, many don't have names at all.
   self.name_index = 0
+  
+  # This takes a bit of explaining but it's important. This number is tracked by
+  # each map to indicate how many treasures exist on this map AND MAPS "BEFORE" IT
+  # in the same world (overworld/undermoon). I'm not sure why this is tracked
+  # manually and not dynamically computed based on the triggers, but here we are.
+  # In any case, if this number does not accurately match the actual treasure count,
+  # it can cause glitches like being able to get treasures by talking to doors etc.
+  # A function will be provided for gamingway to automatically compute the treasure
+  # indexes for all the maps, but as the original game also contains the occasional
+  # error in this number, calling that function will cause a data mismatch even if
+  # you haven't made any changes to any maps (though it will likely be a change for
+  # the better on balance as it will FIX existing glitches as opposed to introducing
+  # new ones).
   self.treasure_index = 0
+  
+  # This indicates how often you fight random battles on this map when stepping on
+  # tiles that have random encounters enabled.
   self.encounter_rate = 0
-  self.encounter_set = 0  # Where is this read from?
+  
+  # There is a list of encounter tables somewhere; this indicates which of those
+  # tables is used for random encounters generated by this map.
+  self.encounter_set = 0
 
  def read(self, rom, address):
   self.backdrop = rom.data[address] % 0x10
@@ -216,28 +326,51 @@ class Map:
  def write_encounter_rate(self, rom, address):
   rom.data[address] = self.encounter_rate
  
+ # Returns a string representing the map information.
  def display(self, main):
   result = ""
+  
+  # Produce an error if the name index is out of bounds.
   if self.name_index > len(main.map_names):
    print("ERROR: Name index {} / {}".format(self.name_index, len(main.map_names)))
+
+  # Otherwise, simply return the map name for now.
+  # I will likely flesh this out later to show more information, but for now I just
+  # need to be able to know which map is which.
   else:
    result = main.map_names[self.name_index]
+   
+  # And return the resulting string.
   return result
 
+# Read the list of map names from the rom.
 def read_map_names(rom, text):
+
+ # Start with a list of blanks.
  map_names = [""] * rom.TOTAL_MAP_NAMES
  address = rom.MAP_NAMES_START
+ 
+ # Then read through the list of map names.
+ # There's no pointer table; the names are just separated by 00 bytes.
  for index in range(rom.TOTAL_MAP_NAMES):
+ 
+  # Start with a blank name, then read bytes one by one, adding the corresponding
+  # symbols to the name until we read a 00 byte.
   name = ""
   while rom.data[address] > 0:
    name += chr(rom.data[address])
    address += 1
+  
+  # Finally, we convert the name to ascii to make it easier to work with.
   address += 1
   map_names[index] = text.asciitext(name)
+  
+ # And return the completed list.
  return map_names
 
+# Write the map names back to the rom.
 def write_map_names(rom, text, map_names):
- # Write the map names back to the rom.
+
  # Since this is variable-length data, we need to make sure there is enough room
  # before we write it in order to prevent data bleed.
  room_needed = 0
@@ -267,8 +400,10 @@ def write_map_names(rom, text, map_names):
    rom.data[address] = 0
    address += 1
 
+# This reads the Map data from the rom and returns the resulting list.
 def read_maps(rom):
- # This reads the Map data from the rom and returns the resulting list.
+
+ # Start with an empty list.
  maps = []
  
  # We simply loop through the list of Maps in the rom and use the Map object's
@@ -292,8 +427,9 @@ def read_maps(rom):
  # And finally, return the list.
  return maps
 
+# This writes the Map data back to the rom.
 def write_maps(rom, maps):
- # This writes the Map data back to the rom.
+
  # We simply loop through the list of Maps and write each one using the Map object's
  # "write" method.
  for index, map in enumerate(maps):
@@ -305,10 +441,12 @@ def write_maps(rom, maps):
   # have to pass it several different addresses.
   map.write_encounter_rate(rom, rom.ENCOUNTER_RATES_START + index)
 
+# This reads the list of TileMaps from the rom.
 def read_tilemaps(rom):
- # This reads the list of TileMaps from the rom.
+
  # The "oldaddress" variable is there to track the previous TileMap's pointer
- # because when a TileMap has no data, it seems to use the previous TileMap's pointer.
+ # because when a TileMap has no data, it seems to use the previous TileMap's
+ # pointer, not a pointer to where the previous TileMap's data left off.
  oldaddress = None
  tilemaps = []
  
@@ -335,8 +473,9 @@ def read_tilemaps(rom):
  # And finally we return the list.
  return tilemaps
 
+# This writes the list of tilemaps back to the rom.
 def write_tilemaps(rom, tilemaps):
- # This writes the list of tilemaps back to the rom.
+
  # The "oldaddress" variable is there to track the previous TileMap's pointer
  # because when a TileMap has no data, it seems to use the previous TileMap's pointer,
  # even if the previous map had data. Thus we can't simply use the address where we
@@ -366,9 +505,10 @@ def write_tilemaps(rom, tilemaps):
   # off in the process.
   address = tilemap.write(rom, address)
 
+# This reads the RLE-encoded overworld tile data and returns it as an array.
 def read_overworld(rom):
- # This reads the RLE-encoded overworld tile data and returns it as an array.
- # The array itself is one-dimensional but in practice it is 256x256.
+
+ # The array itself is one-dimensional but it represents a 256x256 grid.
  tilemap = TileMap()
  address = rom.OVERWORLD_DATA_START
  tiles = []
@@ -416,6 +556,8 @@ def read_overworld(rom):
      address += 1
      for index in range(runlength):
       tilemap.tiles[row].append(tile % 0x80)
+    
+    # Otherwise, it's a single tile and we can simply add it to the list directly.
     else:
      tilemap.tiles[row].append(tile)
 
@@ -426,9 +568,9 @@ def read_overworld(rom):
  # And finally return the fully parsed tilemap.
  return tilemap
  
+# This writes the 2D array of overworld tiles back to the rom data as a list
+# of 256-tile-wide RLE-encoded rows, along with the appropriate pointers.
 def write_overworld(rom, tilemap):
- # This writes the 2D array of overworld tiles back to the rom data as a list
- # of 256-tile-wide RLE-encoded rows, along with the appropriate pointers.
  address = rom.OVERWORLD_DATA_START
  
  # We go row by row and write each line along with its pointer.
