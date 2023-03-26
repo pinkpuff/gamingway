153faaa977c140bcd669014b3dcddf0e28184700
pinkpuff <kalel.of.krypton@gmail.com>
2023-03-26 08:06:42 -0230

    Added more comments

--- magic.py
+++ magic.py
@@ -5,15 +5,69 @@
 class Spell:
  
  def __init__(self):
+  
+  # The name of the spell in ASCII. Symbols like magic orbs use the corresponding
+  # three letter code in square brackets (e.g. [BLK], [WHT], [SUM]). See text.py
+  # for the full list of codes.
   self.name = ""
+  
+  # This is how long between inputting the command and the spell resolving. Slower
+  # spells have a higher delay value.
   self.delay = 0
+  
+  # There are eight target types. This indicates which of the eight the spell uses.
+  # The target types are enumerated in config.py.
   self.target = 0
+  
+  # The specific function of the spell's power depends on its effect type, but in
+  # general, the higher this number is, the "stronger" the spell is. So damage
+  # spells with higher power will deal more damage; healing spells with higher
+  # power will heal more, etc. Some spell effects don't use it at all though.
   self.power = 0
+  
+  # This indicates the chance of the spell succeeding when cast. Even if a given
+  # target isn't immune to the spell's effect, it still has a chance of failing.
+  # For many (most?) spells, the chance of it failing is either 0 or close enough
+  # to it that you don't have to consider it, especially since I think the caster's
+  # spell power gets added to this number before making the check. However, I think
+  # the target's magic evade is also subtracted from it (or otherwise reduces it
+  # somehow) so sometimes you might see, for example, a spell from a late game
+  # monster appear to do nothing; this could be because it missed due to your
+  # character's magic evasion.
   self.hit = 0
+  
+  # This flag is set for spells that are usable on bosses. If the flag is unset,
+  # monsters that have the Boss flag set will be immune to the spell.
   self.hitsboss = True
+  
+  # This indicates which spell effect the spell has. There is a large list of
+  # effects, and this number represents the index in that list. See config.py for
+  # the full list of spell effects.
   self.effect = 0
+  
+  # When this flag is set, if the spell deals damage, it will deal a lot more than
+  # if this flag is unset. Most, if not all, of the vanilla damage spells have it
+  # set, so if you set it, it will do the kind of damage you would probably expect.
+  # I'm not sure what effect it has, if any, on non-damage spells.
   self.damaging = False
+  
+  # An attribute table is a set of flag settings for all the game's attribute
+  # flags. These are things like "fire", "ice", etc, but also statuses like "mute",
+  # "blind", "charm", etc. There is a list of these attribute tables in the rom and
+  # things like spells and equipment use an index in this list rather than each one
+  # having their own independent attribute table. So for example, say attribute
+  # table 3 has the "ice" flag set and all other flags unset, then any spell or 
+  # equipment that uses attribute index 3 will be considered to have the "ice" flag
+  # and no other flags set. The meaning of having particular attribute flags set or
+  # unset depends on what effect the spell has. For example, the "Add status"
+  # effect will try to add all the statuses in the given attribute table whose 
+  # flags are set. In contrast, the "Remove status" effect will try to remove all 
+  # statuses from the target whose corresponding attribute flags are UNSET in the
+  # spell's attribute table.
   self.attributes = 0
+  
+  # This flag indicates whether or not a player unit hit with the spell will show
+  # the "impact" animation (the one that looks like they're getting hit).
   self.impact = False
   self.mp = 0
   self.reflectable = False
