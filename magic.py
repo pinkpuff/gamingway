a6590ab7f66f984575e971b93632e7448629c292
pinkpuff <kalel.of.krypton@gmail.com>
2023-03-26 11:15:19 -0230

    Added more comments

--- magic.py
+++ magic.py
@@ -336,6 +336,7 @@ class Spellbook:
   offset += 1
   return address + offset
 
+ # Return a string containing all the information for this spellbook.
  def display(self, main):
   result = ""
   if len(self.spells) > 0:
@@ -373,7 +374,12 @@ class Spellbook:
   if not spell in self.spells[level]:
    self.spells[level].append(spell)
 
+# Read all the spells from the rom.
 def read_spells(rom, text):
+
+ # For the most part, we just create spells and add them to the list and use the
+ # Spell object's read function. However, there are several different types of data
+ # that have to be read separately.
  spells = []
  for index in range(rom.TOTAL_SPELLS):
   spell = Spell()
@@ -385,6 +391,7 @@ def read_spells(rom, text):
   spell.read_sound(rom, rom.SPELL_SOUNDS_START + index)
  return spells
 
+# Write all the spells back to the rom.
 def write_spells(rom, text, spells):
  for index, spell in enumerate(spells):
   nameoffset = index * rom.SPELL_NAME_WIDTH
@@ -393,22 +400,33 @@ def write_spells(rom, text, spells):
   spell.write_visuals(rom, rom.SPELL_VISUALS_START + index * 4)
   spell.write_sound(rom, rom.SPELL_SOUNDS_START + index)
 
+# Read all the spellbooks from the rom.
 def read_spellbooks(rom, spells = []):
  spellbooks = []
+
+ # We start by reading all the starting spells for all the spellbooks.
  address = rom.STARTING_SPELLS_START
  for index in range(rom.TOTAL_SPELLBOOKS):
   spellbook = Spellbook(spells)
   spellbooks.append(spellbook)
   address = spellbook.read_starting_spells(rom, address)
+
+ # After that, we read all the spell progressions for all the spellbooks.
  address = rom.SPELL_PROGRESSIONS_START
  for index in range(rom.TOTAL_SPELLBOOKS):
   address = spellbooks[index].read_spell_progression(rom, address)
+
+ # And return the list of spellbooks.
  return spellbooks
 
+# Write all the spellbooks back to the rom.
 def write_spellbooks(rom, spellbooks):
- # First, verify there is room for all the progressions.
+
+ # This is variable-length data, so we first need to verify there is room for all
+ # the progressions.
  room_needed = 0
  for spellbook in spellbooks:
+
   # The terminating FF counts as one byte.
   room_needed += 1
 
