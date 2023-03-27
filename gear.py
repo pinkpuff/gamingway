9585a719bbf0e10f055e042b3fc1f16f25c242f0
pinkpuff <kalel.of.krypton@gmail.com>
2023-03-27 11:01:53 -0230

    Added more comments

--- gear.py
+++ gear.py
@@ -253,27 +253,92 @@ class Armor(Equipment):
 class Weapon(Equipment):
 
  def __init__(self):
+
+  # It has all the stats an Equipment has.
   super().__init__()
+
+  # This flag indicates whether or not the weapon can be thrown using Dart.
   self.throwable = False
+
+  # This flag indicates whether the weapon ignores the usual penalties for attacking
+  # from the back row.
   self.ranged = False
+
+  # I have no idea what the function of this flag is. The reason it's called
+  # "litarrow" and not simply "mystery flag" like many of the others is because
+  # there are already a few "mystery flags" in the weapon record, and because in
+  # vanilla, there is exactly one weapon that has this flag set: the Lit Arrow.
   self.litarrow = False
+
+  # Technically another mystery flag, but this one seems to be off on all weapons in
+  # vanilla. I have no idea if setting it has any effect on gameplay.
   self.unused_property = False
+
+  # This flag seems to be set on hammers, but I'm not convinced that the game
+  # actually checks it. My understanding is that anything that needs to check types
+  # of weapons such as hammers, harps, bows, etc, seems to have hard coded item
+  # index ranges and doesn't actually check the flag settings.
   self.hammer_flag = False
+
+  # As above but for axe type weapons.
   self.axe_flag = False
+
+  # My understanding is that a character wielding a weapon with this flag cannot
+  # deal critical hits. 
   self.nerf_crits = False
+
+  # The bonus the weapon gives to the character's physical attack power.
   self.attack = 0
+  
+  # The bonus the weapon gives to the character's physical hit rate.
   self.hit = 0
+
+  # I have no idea what this flag does, if anything.
   self.mystery_flag = False
+
+  # This is the index of the spell this weapon will cast if it is selected from the
+  # character's equipped items in battle. 
   self.casts_spell = 0
+
+  # I have no idea what this flag does either, if anything.
   self.mystery_flag2 = False
+
+  # If this flag is set, the weapon will be consumed when used. This is generally
+  # meant for arrows but there's no reason it couldn't theoretically be set on an
+  # otherwise normal weapon if you wanted to do that.
   self.consumable = False
+
+  # Throwable weapons also have this flag set. I don't think it does anything and
+  # all the "work" is done by the first throwable flag above.
   self.throwable2 = False
+
+  # This is the type of graphic used by the weapon when you attack with it. For
+  # example, does it look like a spear, knife, harp, etc.
   self.sprite = 0
+
+  # This is the index into the list of palettes that determines the colors of the
+  # weapon when animating the user's physical attack.
   self.palette = 0
+
+  # This determines the type of animation used by the weapon. For example, is it
+  # "swung" over the head like a knife or sword? Is it "pulled" like a harp or bow?
+  # As usual, this is determined by an index into a list of weapon swing animations.
   self.swing = 0
+
+  # This is the animation that appears on the target when you hit with the weapon.
+  # For example, the swords' slashing effect, the impact animations of the hammers,
+  # notes of the harp, etc.
   self.slash = 0
-  self.casts_visual = 0 # Seems like these aren't being read yet?
-  self.casts_power = 0  # ^^^
+
+  # This is the animation used by the weapon's "item cast" spell. Normally this will
+  # match the animation used by the spell itself, but there's no reason you couldn't
+  # have a different spell's animation if you felt like doing that.
+  self.casts_visual = 0
+
+  # This is the spell power of the weapon's "item cast" spell. It ignores the user's
+  # spell power and uses this value instead. This is currently not being read from
+  # the rom yet.
+  self.casts_power = 0
 
  def read(self, rom, address):
   self.magnetic = rom.flag(address, 7)
